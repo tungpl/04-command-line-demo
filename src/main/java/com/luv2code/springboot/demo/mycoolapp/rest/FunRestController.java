90cac5371c15a4cb11f8422ed6877ad5956c0536
ITSERVICES\tp640d <tp640d@att.com>
2019-03-05 14:55:18 -0800

    Config application.properties

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,27 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// inject Properties for: coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	// expose new enpoint for "teaminfo"
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo( ) {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
 	
 	// expose "/" that return "Hello World"
 	
